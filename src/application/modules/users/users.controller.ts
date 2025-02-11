0e82767f50ff3d17f6f47781ceed83f5bfe5bcd9
MuryloSaladino <murylosaladino@gmail.com>
2025-02-11 13:27:31 -0300

    feat: user creation route and service

--- src/application/modules/users/users.controller.ts
+++ src/application/modules/users/users.controller.ts
@@ -1,8 +1,18 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, Inject, Post } from "@nestjs/common";
+import UsersService from "./users.service";
+import CreateUserDTO from "./dto/create-user.dto";
 
 @Controller("/users")
 export default class UsersController {
     constructor(
-        
+        @Inject()
+        private readonly usersService: UsersService
     ) {}
+
+    
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    public async createUser(@Body() payload: CreateUserDTO) {
+        return this.usersService.create(payload);
+    }
 }
--- src/application/modules/users/users.service.ts
+++ src/application/modules/users/users.service.ts
@@ -1,8 +1,18 @@
 import { Inject, Injectable } from "@nestjs/common";
+import UsersRepository from "src/infrastructure/repositories/users.repository";
+import CreateUserDTO from "./dto/create-user.dto";
+import UserPresenter from "./dto/user-presenter.dto";
 
 @Injectable()
 export default class UsersService {
     constructor(
-        private readonly 
+        @Inject()
+        private readonly usersRepository: UsersRepository
     ) {}
+
+
+    public async create(payload: CreateUserDTO): Promise<UserPresenter> {
+        const user = await this.usersRepository.create(payload);
+        return UserPresenter.fromUser(user);
+    }
 }
