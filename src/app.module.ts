7e810531811fc46e3d5bf67bc99bbcf17ebb2ed0
MuryloSaladino <murylosaladino@gmail.com>
2025-02-11 13:27:11 -0300

    add: modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import UsersModule from './application/modules/users/users.module';
+import { EnvironmentConfigModule } from './infrastructure/config/enviroment/enviroment-config.module';
 
 @Module({
     imports: [
+        EnvironmentConfigModule,
+        UsersModule
     ],
     controllers: [],
     providers: [],
