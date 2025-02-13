2f9ac1bc55c7ffccfc1a60280b409dc1a41256f2
MuryloSaladino <murylosaladino@gmail.com>
2025-02-13 14:52:14 -0300

    module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
-import UsersModule from './application/modules/users/users.module';
 import { EnvironmentConfigModule } from './infrastructure/config/enviroment/enviroment-config.module';
+import { JwtModule } from '@nestjs/jwt';
+import LoggerModule from './infrastructure/services/logger/logger.module';
+import UseCasesProxyModule from './infrastructure/usecases-proxy/usecases-proxy.module';
+import ControllersModule from './infrastructure/controllers/controllers.module';
+import BCryptModule from './infrastructure/services/bcrypt/bcrypt.module';
+import JWTServiceModule from './infrastructure/services/jwt/jwt.module';
 
 @Module({
     imports: [
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+        }),
+        LoggerModule,
+        UseCasesProxyModule.register(),
+        ControllersModule,
+        BCryptModule,
+        JWTServiceModule,
         EnvironmentConfigModule,
-        UsersModule
     ],
     controllers: [],
     providers: [],
