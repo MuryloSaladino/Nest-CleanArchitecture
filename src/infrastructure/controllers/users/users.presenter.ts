4a5d3dd9a9af90a5135ea4325cd50bb9c5872f25
MuryloSaladino <murylosaladino@gmail.com>
2025-02-13 14:59:44 -0300

    feat: user presenter

---
+++ src/infrastructure/controllers/users/users.presenter.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger";
+import UserModel from "src/domain/models/user.model";
+
+export default class UserPresenter {
+    @ApiProperty()
+    readonly id: string;
+    @ApiProperty()
+    readonly createdAt: Date;
+    @ApiProperty()
+    readonly updatedAt: Date;
+    @ApiProperty()
+    readonly deletedAt: Date | null;
+    @ApiProperty()
+    readonly username: string;
+    @ApiProperty()
+    readonly email: string;
+
+    constructor(user: UserModel) {
+        this.id = user.id;
+        this.createdAt = user.createdAt;
+        this.updatedAt = user.updatedAt;
+        this.deletedAt = user.deletedAt;
+        this.username = user.username;
+        this.email = user.email;
+    }
+}
