aaff4651964c614565aa6a49617fa55ce66aca92
MuryloSaladino <murylosaladino@gmail.com>
2025-02-18 11:02:37 -0300

    refact: params names for better readbility

--- src/domain/repositories/base.repository.ts
+++ src/domain/repositories/base.repository.ts
@@ -4,7 +4,7 @@ import BaseModel from "../models/base.model";
 export default interface IBaseRepository<T extends BaseModel> {
     create(entity: Partial<T>): Promise<T>;
     findById(id: string): Promise<T | null>;
-    findAll(limit?: number, offset?: number): Promise<Paginated<T>>;
+    findAll(page?: number, size?: number): Promise<Paginated<T>>;
     update(id: string, payload: Partial<T>): Promise<T | null>;
     delete(id: string): Promise<void>;
 }
