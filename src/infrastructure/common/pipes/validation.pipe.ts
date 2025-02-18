5a6202f936432c44f605b12d9cad4a5b6c4130eb
MuryloSaladino <murylosaladino@gmail.com>
2025-02-18 11:03:22 -0300

    feat: transforming validation errors for response

--- src/infrastructure/common/pipes/validation.pipe.ts
+++ src/infrastructure/common/pipes/validation.pipe.ts
@@ -1,8 +1,15 @@
 import { ValidationPipe } from "@nestjs/common";
+import { ValidationError } from "class-validator";
+import BadRequestError from "src/infrastructure/errors/bad-request.error";
 
 const validationPipe = new ValidationPipe({
     whitelist: true,
     transform: true,
+    exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        return new BadRequestError(
+            validationErrors.flatMap(x => Object.values(x.constraints!))
+        );
+    }
 });
 
 export default validationPipe;
