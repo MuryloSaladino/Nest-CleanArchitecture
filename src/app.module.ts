9831de6ee061f3b32cca4de0311bf4afcdf5eeee
MuryloSaladino <murylosaladino@gmail.com>
2025-03-11 22:05:16 -0300

    chore: organized modules and exports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,28 @@
 import { Module } from '@nestjs/common';
 import { EnvironmentConfigModule } from './infrastructure/config/enviroment/enviroment-config.module';
 import { JwtModule } from '@nestjs/jwt';
-import LoggerModule from './infrastructure/services/logger/logger.module';
-import UseCasesProxyModule from './infrastructure/usecases-proxy/usecases-proxy.module';
-import ControllersModule from './infrastructure/controllers/controllers.module';
-import BCryptModule from './infrastructure/services/bcrypt/bcrypt.module';
-import JWTServiceModule from './infrastructure/services/jwt/jwt.module';
+import { UsersUseCasesProxyModule } from './infrastructure/usecases-proxy/users.usecases-proxy.module';
+import { LoggerModule } from './infrastructure/services/logger/logger.module';
+import { ControllersModule } from './infrastructure/controllers/controllers.module';
+import { BCryptModule } from './infrastructure/services/bcrypt/bcrypt.module';
+import { JWTModule as JWTServiceModule } from './infrastructure/services/jwt/jwt.module';
 
 @Module({
     imports: [
-        JwtModule.register({
-            secret: process.env.JWT_SECRET,
-        }),
-        LoggerModule,
-        UseCasesProxyModule.register(),
-        ControllersModule,
-        BCryptModule,
-        JWTServiceModule,
+        UsersUseCasesProxyModule.register(),
+
         EnvironmentConfigModule,
+        
+        JwtModule.register({ secret: process.env.JWT_SECRET }),
+        JWTServiceModule,
+        
+        BCryptModule,
+        LoggerModule,
+        
+        ControllersModule,
     ],
     controllers: [],
     providers: [],
     
 })
-export default class AppModule {}
+export class AppModule {}
