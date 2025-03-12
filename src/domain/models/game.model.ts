be16cbd0a17718579aabdd1c167740842d6b4a04
MuryloSaladino <murylosaladino@gmail.com>
2025-03-11 22:04:58 -0300

    feat: model relations

--- src/domain/models/game.model.ts
+++ src/domain/models/game.model.ts
@@ -1,7 +1,9 @@
 import { BaseModel } from "./base.model";
+import { PlayModel } from "./play.model";
 
 export class GameModel extends BaseModel {
     name: string;
     year: number;
     description: string;
+    playedBy: PlayModel[];
 }
--- src/domain/models/user.model.ts
+++ src/domain/models/user.model.ts
@@ -1,7 +1,9 @@
 import { BaseModel } from "./base.model";
+import { PlayModel } from "./play.model";
 
 export class UserModel extends BaseModel {
     username: string;
     email: string;
     password: string;
+    plays: PlayModel[];
 }
