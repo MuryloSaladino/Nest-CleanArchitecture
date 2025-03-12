72c394a623c7358f6d3fc8e929b3fdb35a404156
MuryloSaladino <murylosaladino@gmail.com>
2025-03-11 22:02:47 -0300

    refact: user entity name and default exports removed

--- src/infrastructure/repositories/repositories.module.ts
+++ src/infrastructure/repositories/repositories.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
-import UsersRepository from './users.repository';
-import { TypeOrmConfigModule } from '../config/typeorm/typeorm-config.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import User from '../entities/user.entity';
+import { UsersRepository } from './users.repository';
+import { UserEntity } from '../entities/user.entity';
+import { TypeOrmConfigModule } from '../config/typeorm/typeorm.module';
 
 @Module({
-    imports: [TypeOrmConfigModule, TypeOrmModule.forFeature([User])],
-    providers: [UsersRepository],
-    exports: [UsersRepository]
+    imports: [
+        TypeOrmConfigModule, 
+        TypeOrmModule.forFeature([UserEntity])
+    ],
+    providers: [
+        UsersRepository, 
+    ],
+    exports: [
+        UsersRepository, 
+    ]
 })
-export default class RepositoriesModule {}
+export class RepositoriesModule {}
--- src/infrastructure/repositories/users.repository.ts
+++ src/infrastructure/repositories/users.repository.ts
@@ -1,19 +1,19 @@
 import { InjectRepository } from "@nestjs/typeorm";
 import { IUsersRepository } from "src/domain/repositories/user.repository";
-import User from "../entities/user.entity";
 import { Repository } from "typeorm";
-import BaseRepository from "./base-repository";
 import { Injectable } from "@nestjs/common";
+import { UserEntity } from "../entities/user.entity";
+import { BaseRepository } from "./base-repository";
 
 @Injectable()
-export default class UsersRepository extends BaseRepository<User> implements IUsersRepository {
+export class UsersRepository extends BaseRepository<UserEntity> implements IUsersRepository {
     constructor(
-        @InjectRepository(User)
-        protected readonly repository: Repository<User>
+        @InjectRepository(UserEntity)
+        protected readonly repository: Repository<UserEntity>
     ) { super() }
 
 
-    public async findOneByEmail(email: string): Promise<User | null> {
+    public async findOneByEmail(email: string): Promise<UserEntity | null> {
         return await this.repository.findOne({
             where: { email }
         });
